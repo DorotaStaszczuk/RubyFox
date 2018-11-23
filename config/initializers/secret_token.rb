259965e0e76a54e0328a3819c37692183e14fee7
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-11-23 10:02:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyFox::Application.config.secret_key_base = 'aff323c65ef669fcc9fc5c201e986cbd1db364ecc55ec8d7cdbab2cab0d257f9234bd7490b56660ee0901d955223d1f65ab3f1e86f8f2b28b09161e050151941'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyFox::Application.config.session_store :cookie_store, key: '_RubyFox_session'
