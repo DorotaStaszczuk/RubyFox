83e26ff210f19975c8fa3ca9d8f4b95e0a724c5e
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-11-26 12:31:50 +0100

    user_name added to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:description, :image_url)
+      params.require(:user).permit(:user_name, :description, :image_url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126083332) do
+ActiveRecord::Schema.define(version: 20181126102351) do
 
   create_table "photos", force: true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20181126083332) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_name"
   end
 
 end
